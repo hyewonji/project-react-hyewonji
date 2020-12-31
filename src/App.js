dd030072407f7230de018e5b423dd2f93f920a09
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-12-31 11:52:28 +0900

    Build Login Form And Implement Submit Function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import React from "react";
-import Login from "./routes/Login";
+import React, { Component } from "react";
+import LoginTemplate from "./components/LoginTemplate";
+import Form from "./components/Form"
 
-function App() {
-  return (
-    <div className="App">
-      <Login></Login>
-    </div>
-  );
+class App extends Component {
+  id = 1
+  userInfo = {
+    email: 'hyewon@gmail.com',
+    password: '123456'
+  }
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  handleSubmit = () => {
+    const { email, password } = this.state;
+    if (this.state.email === this.userInfo.email && this.state.password === this.userInfo.password) {
+      alert("Login Success!")
+    } else if (this.state.email === this.userInfo.email && this.state.password !== this.userInfo.password) {
+      alert("Check Your Password")
+    } else {
+      alert("Check Your Email")
+    }
+  }
+
+  render() {
+    const { email, password } = this.state;
+    const {
+      handleSubmit,
+      handleEmailChange,
+      handlePasswordChange
+    } = this;
+    return (
+      <LoginTemplate form={<Form
+        email={email}
+        password={password}
+        onEmailChange={handleEmailChange}
+        onPasswordChange={handlePasswordChange}
+        onSubmit={handleSubmit}
+      />}>
+
+      </LoginTemplate>
+    );
+  }
 }
 /*
 component : HTML을 반환하는 함수 
