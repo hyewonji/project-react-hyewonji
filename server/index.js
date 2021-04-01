0ebf6f5a7d69a5b46792a4d57743a4d5ea1fde7c
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-04-02 00:24:31 +0900

    Heroku 배포 404 오류 수정 Production 사용시 Port 번호를 환경변수로 지정했다. Heroku Config Vars를 지정해줘서 MongoDB를 연결할 수 있게 했다.

--- server/index.js
+++ server/index.js
@@ -98,7 +98,7 @@ app.get('api/users/logout', auth, (req,res) => {
 });
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ( ) => {
   console.log(`Example app listening at http://localhost: ${port}`);
